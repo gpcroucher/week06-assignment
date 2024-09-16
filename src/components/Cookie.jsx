8e9ffef2d68eb992edd736ef67aa03d39d00b1af
George Croucher <gpcroucher@gmail.com>
2024-09-16 14:54:17 +0100

    title and cookie style

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -2,7 +2,9 @@ import PropTypes from "prop-types";
 import "./Cookie.css";
 
 export default function Cookie(props) {
-  return <button onClick={props.handleClick}></button>;
+  return (
+    <button className={"cookieButton"} onClick={props.handleClick}></button>
+  );
 }
 
 Cookie.propTypes = {
